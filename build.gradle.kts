2fd04bc4afcb120f20fe3ba90f179b247ff76928
Connor Haskins <ConnorAHaskins@gmail.com>
2024-01-30 17:28:32 -0800

    gradle publishing configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.Lucodivo"
-version = "v0.2.1-alpha"
+version = "v0.2.2-alpha"
 
 repositories {
     mavenCentral()
@@ -14,9 +14,56 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("mavenCentral") {
+            groupId = "com.github.Lucodivo"
+            artifactId = "NoopMathKt"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("NoopMathKt")
+                description.set("Linear Algebra Kotlin library for use with OpenGL")
+                url.set("https://github.com/lucodivo/NoopMathKt")
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://choosealicense.com/licenses/apache-2.0/")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("lucodivo")
+                        name.set("Connor A. Haskins")
+                        email.set("ConnorAHaskins@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/lucodivo/NoopMathKt.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:lucodivo/NoopMathKt.git")
+                    url.set("https://github.com/lucodivo/NoopMathKt")
+                }
+            }
+        }
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(17)
 }
